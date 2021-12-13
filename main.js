a3b9745e9fd77101eacb2529681f51244430dac6
Dmitry Mass <dimamikron41@gmail.com>
2021-12-13 13:53:06 +0300

    Added new file

---
+++ main.js
@@ -0,0 +1,63 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// const newArray = numbers.filter(filterCallback);
+
+// console.log(newArray);
+
+// function filterCallback(item) {
+//   if (item >= 5 && item <= 100) {
+//     return item;
+//   }
+// }
+
+//////////////////
+
+// function filter(arr, callback) {
+//   const newArr = [];
+
+//   for (let i = 0; i < arr.lenght; i++) {
+//     const elem = callback(arr[i], i, arr);
+
+//     if (elem !== undefined) {
+//       newArr.push(elem);
+//     }
+//   }
+//   return newArr;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 8, 9, 999, 992, 990];
+
+// const result = filter(numbers, function (item) {
+//   if (item >= 5 && item <= 1000) {
+//     return item;
+//   }
+// });
+
+// console.log(result);
+
+// every and some and find;
+
+// reduce
+
+let string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolore, suck , corporis quibusdam, fuck , velit quia et illo. Ut, sed debitis tempora dicta rem dolorem, corrupti officiis blanditiis unde perspiciatis et a!`;
+
+function nameFilter(words) {
+  let string = words.toLowerCase().split(" ");
+
+  let result = string.filter(function (item) {
+    if (item !== "fuck" && item !== "suck") {
+      return item;
+    }
+  });
+  return result.join(" ");
+}
+
+let endResult = nameFilter(string);
+console.log(endResult);
+
+// (
+//   item !== "fuck".toUpperCase() &&
+//   item !== "fuck".toLowerCase() &&
+//   item !== "suck".toUpperCase() &&
+//   item !== "suck".toLowerCase()
+// )
